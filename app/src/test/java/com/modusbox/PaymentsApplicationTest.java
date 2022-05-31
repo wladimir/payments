56c0d05d92c72ca91f9d0a6d5185efe013780dff
Vladimir Ćirković <vladimir.cirkovic@icloud.com>
2022-05-31 17:40:07 +0200

    Initial commit

---
+++ app/src/test/java/com/modusbox/PaymentsApplicationTest.java
@@ -0,0 +1,21 @@
+package com.modusbox;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PaymentsApplicationTest {
+
+	@Autowired
+	private MyService myService;
+
+	@Test
+	public void contextLoads() {
+		assertThat(myService.message()).isNotNull();
+	}
+
+}
