7aa651e840f320a4298f1e282f7435a8b02da5b4
Vladimir Ćirković <vladimir.cirkovic@icloud.com>
2022-06-01 17:04:46 +0200

    Further setup

--- app/src/main/java/com/modusbox/PaymentsApplication.java
+++ app/src/main/java/com/modusbox/PaymentsApplication.java
@@ -2,23 +2,9 @@ package com.modusbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(scanBasePackages = "com.modusbox")
-@RestController
 public class PaymentsApplication {
-    private final MyService myService;
-
-    public PaymentsApplication(MyService myService) {
-        this.myService = myService;
-    }
-
-    @GetMapping("/")
-    public String home() {
-        return myService.message();
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(PaymentsApplication.class, args);
     }
